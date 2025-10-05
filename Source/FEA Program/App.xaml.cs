aa5ed2993b97e4b981c9e16eaca6109410432d50
Graham <kerr.graham.d@gmail.com>
2025-10-05 12:44:09 -0700

    Add unhandled error handling

--- Source/FEA Program/App.xaml.cs
+++ Source/FEA Program/App.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace FEA_Program
+﻿using System.Windows;
+
+namespace FEA_Program
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        /// <summary>
+        /// Fires when an unhandled exception occurs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            System.Windows.MessageBox.Show($"An unhandled exception just occurred: {e.Exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 
 }
