26daa3c0cb7444203de26bce1fca8c0dfae7a72c
Graham <kerr.graham.d@gmail.com>
2025-10-25 14:52:04 -0700

    Color results with invalid values

---
+++ Source/FEA Program/Converters/BoolToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FEA_Program.Converters
+{
+    /// <summary>
+    /// Converts a boolean value to a color
+    /// </summary>
+    internal class BoolToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; } = Colors.Black;
+        public Color FalseColor { get; set; } = Colors.Red;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = (bool)value;
+
+            Color colorToUse = bValue ? TrueColor : FalseColor;
+            return new SolidColorBrush(colorToUse);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException(); // One-way only
+        }
+    }
+}
