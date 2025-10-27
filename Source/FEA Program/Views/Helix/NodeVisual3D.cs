da10f52857df3fbb097ba33b26d438d7a219ebb2
Graham <kerr.graham.d@gmail.com>
2025-10-26 22:57:37 -0700

    Cleanup text implementation

--- Source/FEA Program/Views/Helix/NodeVisual3D.cs
+++ Source/FEA Program/Views/Helix/NodeVisual3D.cs
@@ -9,8 +9,6 @@ namespace FEA_Program.Views.Helix
 {
     internal class NodeVisual3D : GroupModel3D
     {
-        private readonly BillboardSingleText3D _textGeometry;
-        private readonly BillboardTextModel3D _textVisual;
 
         public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
             nameof(Color), typeof(Color), typeof(NodeVisual3D),
@@ -19,10 +17,6 @@ namespace FEA_Program.Views.Helix
         public static readonly DependencyProperty PositionProperty = DependencyProperty.Register(
                 nameof(Position), typeof(Vector3), typeof(NodeVisual3D),
                 new PropertyMetadata(new Vector3(), OnPositionChanged));
-
-        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
-                nameof(Text), typeof(string), typeof(NodeVisual3D),
-                new PropertyMetadata("", OnTextChanged));
 
         public Color Color
         {
@@ -34,12 +28,6 @@ namespace FEA_Program.Views.Helix
         {
             get => (Vector3)GetValue(PositionProperty);
             set => SetValue(PositionProperty, value);
-        }
-
-        public string Text
-        {
-            get => (string)GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
         }
 
         public double Size { get; set; } = 1;
@@ -62,33 +50,6 @@ namespace FEA_Program.Views.Helix
             };
 
             Children.Add(box);
-
-            // Create node text
-            _textGeometry = new BillboardSingleText3D()
-            { 
-                FontColor = new Color4(0, 0, 0, 1),
-                FontSize = 12,
-                FontWeight = FontWeights.Regular,
-                BackgroundColor = new Color4(0.8f, 0.8f, 0.8f, 0.5f),
-                Padding = new Thickness(2),
-            };
-
-            _textVisual = new BillboardTextModel3D
-            {
-                Geometry = _textGeometry,
-                Visibility = Visibility.Hidden, // Hide until explicitly shown
-                IsHitTestVisible = false,  // Not clickable
-                FixedSize = false,  // Prevents scaling with zoom 
-            };
-
-            // Keeps the text on top
-            var topVisual = new TopMostGroup3D
-            {
-                IsHitTestVisible = false
-            };
-
-            topVisual.Children.Add(_textVisual);
-            Children.Add(topVisual);
         }
 
         private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -100,12 +61,6 @@ namespace FEA_Program.Views.Helix
         {
             if (d is NodeVisual3D group)
                 group.UpdateTransform();
-        }
-
-        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is NodeVisual3D group)
-                group.UpdateText();
         }
 
         private void UpdateChildrenColor()
@@ -120,12 +75,6 @@ namespace FEA_Program.Views.Helix
         private void UpdateTransform()
         {
             Transform = new TranslateTransform3D(Position.X, Position.Y, Position.Z);
-        }
-
-        private void UpdateText()
-        {
-            _textGeometry.TextInfo = new TextInfo(Text, new Vector3(0.9f, 0.9f, 0.9f)) { Scale = 0.03f };
-            _textVisual.Visibility = Text == "" ? Visibility.Hidden : Visibility.Visible;
         }
     }
 }
---
+++ Source/FEA Program/Views/Helix/TextVisual3D.cs
@@ -0,0 +1,60 @@
+﻿using HelixToolkit.Wpf.SharpDX;
+using SharpDX;
+using System.Windows;
+
+namespace FEA_Program.Views.Helix
+{
+    /// <summary>
+    /// A visual that displays text above all other elements
+    /// </summary>
+    internal class TextVisual3D : TopMostGroup3D
+    {
+        private readonly BillboardSingleText3D _textGeometry;
+        private readonly BillboardTextModel3D _textVisual;
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+                nameof(Text), typeof(string), typeof(TextVisual3D),
+                new PropertyMetadata("", OnTextChanged));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public TextVisual3D()
+        {
+            // Create text
+            _textGeometry = new BillboardSingleText3D()
+            {
+                FontColor = new Color4(0, 0, 0, 1),
+                FontSize = 36,
+                FontWeight = FontWeights.Regular,
+                BackgroundColor = new Color4(0.8f, 0.8f, 0.8f, 0.5f),
+                Padding = new Thickness(2),
+            };
+
+            _textVisual = new BillboardTextModel3D
+            {
+                Geometry = _textGeometry,
+                Visibility = Visibility.Hidden, // Hide until explicitly shown
+                IsHitTestVisible = false,  // Not clickable
+                FixedSize = false,  // Prevents scaling with zoom 
+            };
+
+            Children.Add(_textVisual);
+        }
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextVisual3D group)
+                group.UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            _textGeometry.TextInfo = new TextInfo(Text, new Vector3(0.9f, 0.9f, 0.9f)) { Scale = 0.01f };
+            _textVisual.Visibility = Text == "" ? Visibility.Hidden : Visibility.Visible;
+        }
+    }
+}
