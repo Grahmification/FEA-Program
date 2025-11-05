544e513049e379e2c9141bc6e67c5d3a424aca9c
Graham <kerr.graham.d@gmail.com>
2025-11-04 22:28:55 -0800

    Update node edit position on enter key

--- Source/FEA Program/Views/EditNodeControl.xaml.cs
+++ Source/FEA Program/Views/EditNodeControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace FEA_Program.Views
 {
@@ -10,6 +12,17 @@ namespace FEA_Program.Views
         public EditNodeControl()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Force the textbox to validate when the enter key is pressed
+            if (e.Key == Key.Enter)
+            {
+                var textBox = (TextBox)sender;
+                var binding = BindingOperations.GetBindingExpression(textBox, TextBox.TextProperty);
+                binding?.UpdateSource();
+            }
         }
     }
 }
